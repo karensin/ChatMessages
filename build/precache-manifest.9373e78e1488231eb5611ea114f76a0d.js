10a1617185425cd9a95013577d00b17729ac539d
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-02-19 12:58:29 -0800

    putting vscode folder into gitignore

---
+++ build/precache-manifest.9373e78e1488231eb5611ea114f76a0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c9639eaa3eb5d1164f183bbe28eed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afcf7a66490e3e82b9cb",
+    "url": "/static/css/2.8623a040.chunk.css"
+  },
+  {
+    "revision": "cc826b3f3463a00990ba",
+    "url": "/static/css/main.1286ad82.chunk.css"
+  },
+  {
+    "revision": "afcf7a66490e3e82b9cb",
+    "url": "/static/js/2.c75d1d26.chunk.js"
+  },
+  {
+    "revision": "6b58eaa96151b738ff4fd57523e7eb65",
+    "url": "/static/js/2.c75d1d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc826b3f3463a00990ba",
+    "url": "/static/js/main.6201e9f3.chunk.js"
+  },
+  {
+    "revision": "42a8b147aa10dbde66ec",
+    "url": "/static/js/runtime-main.9f7c15aa.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
